599352cc41ec616888ec553f5ce977044b88e49e
jenkins-coho <jenkins.coho@gmail.com>
2014-07-31 15:22:20 +0100

    harmonise to CHGOVUK standard

--- lib/MojoX/Plugin/Hook/BeforeRendered.pm
+++ lib/MojoX/Plugin/Hook/BeforeRendered.pm
@@ -3,7 +3,7 @@ package MojoX::Plugin::Hook::BeforeRendered;
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::Util qw(monkey_patch);
 
-our $VERSION = "0.03";
+our $VERSION = '0.30';
 my  $ALREADY_REGISTERED=0;
 
 # -----------------------------------------------------------------------------
